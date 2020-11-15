e9f857961d3b163af59fef83add616d2424f2384
Maeesha <maeesha.rahman@gmail.com>
2020-11-15 11:59:31 -0500

    adding files

---
+++ src/pages/Mentorship/Mentorship.page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+require("./mentorship.scss");
+
+const MentorshipPage = () => {
+  return (
+    <main className="mentorship-page">
+      <div></div>
+    </main>
+  );
+};
+
+export default MentorshipPage;
