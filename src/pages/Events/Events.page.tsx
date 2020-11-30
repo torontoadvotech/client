25782458bb315f015bf5697ca4e3ff1cc9f446b0
neilhsieh <neilhsieh.94@gmail.com>
2020-11-29 21:03:25 -0500

    added forms section and mostly styled correctly for desktop

--- src/pages/Events/Events.page.tsx
+++ src/pages/Events/Events.page.tsx
@@ -32,7 +32,35 @@ const EventsPage = () => {
 				</div>
 				<button className="events-all">View All Events</button>
 			</section>
-			<form className="events-form"></form>
+			<section className="form-section">
+				<div className="form-section-header">
+					<h3 className="form-section-header-title">
+						Host an event
+					</h3>
+					<p className="form-section-description">
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Posuere tortor egestas massa
+						scelerisque eleifend quam. Euismod scelerisque eu tempor arcu, sit ornare sit.
+					</p>
+				</div>
+				<form className="events-form">
+					<div className="form-name">
+						<label htmlFor="form-name">Name</label>
+						<input type="text" name="name-input" id="form-name" placeholder="Jenna Hunter" />
+					</div>
+					<div className="form-email">
+						<label htmlFor="form-name">Email</label>
+						<input type="email" name="email-input" id="form-email" placeholder="jenna.hunter@gmail.com" />
+					</div>
+					<div className="form-phone">
+						<label htmlFor="form-phone">Phone</label>
+						<input type="tel" name="form-input" id="form-phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" placeholder="416-111-1111" />
+					</div>
+					<div className="form-request">
+						<label htmlFor="form-request">Summary of Request</label>
+						<textarea name="form-request" id="form-reqeust" placeholder="Hello, I would like to host an event&#10; &#10;Thanks,&#10;&#10;John" ></textarea>
+					</div>
+				</form>
+			</section>
 		</main>
 	)
 }
