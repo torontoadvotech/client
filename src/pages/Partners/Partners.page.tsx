3a73f0203dc3807f83c5e5d924f78ac68bd3a60c
Alexandra Lim <alexandra.lim@gmail.com>
2021-01-03 22:27:54 -0500

    desktop minus modal and img overlay done

--- src/pages/Partners/Partners.page.tsx
+++ src/pages/Partners/Partners.page.tsx
@@ -1,29 +1,42 @@
 import React from "react";
 import PartnersForm from "./PartnersForm";
-import currentPartnersImage from '../../assets/images/current-partners.png';
+import currentPartnersImage from "../../assets/images/current-partners.png";
+
+import "./partners.scss";
 
 const PartnersPage = () => {
   return (
-    <main className="partners-page">
-      <section className="intro-section">
-        <span className="text-slogan">
+    <main className='partners-page'>
+      <section className='intro-section'>
+        <h1 className='text-slogan'>
           Become a <span>Partner</span>
-        </span>
-        <p className="copy">
-          Passionate about shifting social paradigms? Show your support for a gender forward STEM field, and apply to be an official partner of our cause! 
+        </h1>
+        <p className='copy'>
+          Passionate about shifting social paradigms? Show your support for a
+          gender forward STEM field, and apply to be an official partner of our
+          cause!
         </p>
       </section>
-      <section className="current-partners">
+      <section className='current-partners'>
         <figure>
-          <img src={currentPartnersImage} alt=""/>
+          <img src={currentPartnersImage} alt='' />
         </figure>
-        <p className="text-slogan">
-          Meet Our Team
-        </p>
-        <p>Current Partners</p>
-        <p>Our current Team of Partners is dedicated to breaking boundaries and  creating a STEM that is accepting to all. Together, we can all contribute to creating  a healthier work environment. Our current Team of Partners is dedicated to breaking boundaries and  creating a STEM that is accepting to all. Together, we can all contribute to creating  a healthier work environment.</p>
+        <div className='current-partners-text-content'>
+          <h2 className='text-slogan'>Meet Our Team</h2>
+          <h2 className='text-slogan'>
+            Current <span className='red'>Partners</span>
+          </h2>
+          <p>
+            Our current Team of Partners is dedicated to breaking boundaries and
+            creating a STEM that is accepting to all. Together, we can all
+            contribute to creating a healthier work environment. Our current
+            Team of Partners is dedicated to breaking boundaries and creating a
+            STEM that is accepting to all. Together, we can all contribute to
+            creating a healthier work environment.
+          </p>
+        </div>
       </section>
-      <section className="partners-form">
+      <section className='partners-form'>
         <PartnersForm />
       </section>
     </main>
--- src/pages/Partners/PartnersForm.tsx
+++ src/pages/Partners/PartnersForm.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import Modal from '../../components/Modal/Modal';
+import React, { useState } from "react";
+import Modal from "../../components/Modal/Modal";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 
@@ -15,19 +15,18 @@ const PartnersForm = () => {
   const [formSubmit, setFormSubmit] = useState(false);
 
   const initialValues: PartnersType = {
-    companyName: '',
-    industry: '',
-    summaryOfRequest: '',
-    phoneNumber: '',
-    email: '',
-  }
+    companyName: "",
+    industry: "",
+    summaryOfRequest: "",
+    phoneNumber: "",
+    email: "",
+  };
 
   const onSubmit = (values: PartnersType, { resetForm }) => {
     const { companyName } = values;
-    // alert(`hi ${companyName}`);
     setFormSubmit(true);
-    resetForm({ values: '' });
-  }
+    resetForm({ values: "" });
+  };
 
   const closeModal = () => {
     setFormSubmit(false);
@@ -39,85 +38,94 @@ const PartnersForm = () => {
     email: Yup.string()
       .email("Invalid email address")
       .required("Required"),
-    phoneNumber: Yup.string().required('Required'),
+    phoneNumber: Yup.string().required("Required"),
     industry: Yup.string().required("Required"),
     summaryOfRequest: Yup.string().required("Required"),
   });
 
-  return ( 
+  return (
     <>
-    <div className="form-container form-container__partners">
-      <p>Join the Team</p>
-      <p>All submissions will be reviewed by our team, and a response will be sent back within 2-3 business days.</p>
+      <div className='form-container form-container__partners'>
+        <h3>Join the Team</h3>
+        <p>
+          All submissions will be reviewed by our team, and a response will be
+          sent back within 2-3 business days.
+        </p>
 
-      <Formik 
-        initialValues={initialValues} 
-        validationSchema={validationSchema}
-        onSubmit={onSubmit}>
-        <Form>
-          <div className="field-container">
-            <label htmlFor="company-name">Company name</label>
-            <ErrorMessage 
-              component="span" 
-              className="form-error" 
-              name="companyName" 
-            />
-            <Field 
-              name="companyName" 
-            />
-          </div>
-          <div className="field-container">
-            <label htmlFor="email">Email</label>
-            <ErrorMessage 
-              component="span" 
-              className="form-error" 
-              name="email" 
-            />
-            <Field 
-              name="email"
-            />
-          </div>
-          <div className="field-container">
-            <label htmlFor="phone-number">Phone number</label>
-            <ErrorMessage 
-              component="span" 
-              className="form-error" 
-              name="phoneNumber" 
-            />
-            <Field 
-              name="phoneNumber"
-            />
-          </div>
-          <div className="field-container">
-            <label htmlFor="industry">Industry</label>
-            <ErrorMessage 
-              component="span" 
-              className="form-error" 
-              name="industry" 
-            />
-            <Field 
-              name="industry"
-            />
-          </div>
-          <div className="field-container">
-            <label htmlFor="summary-of-request">Summary of request</label>
-            <ErrorMessage 
-              component="span" 
-              className="form-error" 
-              name="summaryOfRequest" 
-            />
-            <Field 
-              name="summaryOfRequest"
-              component="textarea"
-            />
-          </div>
-          <button type="submit" className="button-primary">
-            Submit
-          </button>
-        </Form>
-      </Formik>
-    </div>
-    {formSubmit && <Modal title={'hi'} children={'children'} onClose={closeModal} />}
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={onSubmit}
+        >
+          <Form>
+            <div className='text-inputs'>
+              <div className='field-container'>
+                <label htmlFor='company-name'>Company name</label>
+                <ErrorMessage
+                  component='span'
+                  className='form-error'
+                  name='companyName'
+                />
+                <Field name='companyName' placeholder='John Smith' />
+              </div>
+              <div className='field-container'>
+                <label htmlFor='email'>Email</label>
+                <ErrorMessage
+                  component='span'
+                  className='form-error'
+                  name='email'
+                />
+                <Field name='email' placeholder='johnsmith@gmail.com' />
+              </div>
+              <div className='field-container'>
+                <label htmlFor='phone-number'>Phone number</label>
+                <ErrorMessage
+                  component='span'
+                  className='form-error'
+                  name='phoneNumber'
+                />
+                <Field name='phoneNumber' placeholder='416-111-1111' />
+              </div>
+              <div className='field-container'>
+                <label htmlFor='industry'>Industry</label>
+                <ErrorMessage
+                  component='span'
+                  className='form-error'
+                  name='industry'
+                />
+                <Field name='industry' placeholder='Design' />
+              </div>
+            </div>
+            <div className='textarea'>
+              <div className='field-container'>
+                <label htmlFor='summary-of-request'>Summary of request</label>
+                <ErrorMessage
+                  component='span'
+                  className='form-error'
+                  name='summaryOfRequest'
+                />
+                <Field
+                  name='summaryOfRequest'
+                  component='textarea'
+                  placeholder='Hello, I would like to host an event.&#10;&#10;Thanks,&#10;&#10;John'
+                />
+              </div>
+              <button type='submit' className='button-primary'>
+                Submit
+              </button>
+            </div>
+          </Form>
+        </Formik>
+      </div>
+      {formSubmit && (
+        <Modal
+          title={"Congrats!"}
+          children={
+            "Your request has been sent and is under review by our team. You should receive a response within the next 48 hours!"
+          }
+          onClose={closeModal}
+        />
+      )}
     </>
   );
 };
